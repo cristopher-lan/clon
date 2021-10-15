b64a33d08ba6b19f73b4b70173ffcf89e0ce30a6
cristopher-lan <84140548+cristopher-lan@users.noreply.github.com>
2021-10-15 16:05:20 -0500

    Create registrar.php

---
+++ registrar.php
@@ -0,0 +1,26 @@
+<?php
+include("con_db.php");
+if (isset($_POST['register'])) {
+    if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1){
+  	    $name = trim($_POST['name']);
+        $email = trim($_POST['email']);
+        $fechareg = date("d/m/y");	
+        $consulta = "INSERT INTO datos(nombre, email, fech_registro) VALUES ('$name','$email','$fechareg')";
+        $resultado = mysqli_query($conex,$consulta);
+        if ($resultado) {
+     	    ?>
+     	    <h3 class = "ok">te has escrito correctamente</h3>
+     	   <?php
+        } else {
+     	    ?>
+     	    <h3 class = "bad">Ups ha ocurrido un error</h3>
+     	   <?php
+        }
+    }   else {
+            ?>
+     	    <h3 class = "bad">Por favor complete los campos</h3>
+     	   <?php
+    }
+}
+
+?>
